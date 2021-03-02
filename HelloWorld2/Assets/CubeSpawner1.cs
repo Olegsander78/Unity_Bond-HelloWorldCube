2548a134872280ed5b7948e45908d7da8595bfe5
Olegsander78 <onkotov@gmail.com>
2021-03-02 11:49:26 +0500

    добавлен листинг для проверки Отладчика

---
+++ HelloWorld2/Assets/CubeSpawner1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner1 : MonoBehaviour
+{
+    public GameObject cubePrefabVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(cubePrefabVar);
+
+        bool f = false;
+        bool t = true;
+        print(f == t);
+        print(f = t);
+        print(10 != 10);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Instantiate(cubePrefabVar); 
+    }
+}
---
+++ HelloWorld2/Assets/CubeSpawner2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner2 : MonoBehaviour
+{
+    public GameObject cubePrefabVar;
+    // Start is called before the first frame update
+    void Start()
+    {
+             
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SpellItOut();
+        Instantiate(cubePrefabVar);
+    }
+    public void SpellItOut()
+    {
+        string sA = "Hello world!";
+        string sB = "";
+        for (int i = 0; i < sA.Length; i++)
+        {
+            sB += sA[i];
+        }
+        print(sB);
+    }
+}
